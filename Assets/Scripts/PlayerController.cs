154e94f757269f76ca44fc3d99bb557540ace67b
Logan-Okami <51331444+Okami2004@users.noreply.github.com>
2020-02-27 12:06:22 -0800

    finished lesson 3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,10 @@ public class PlayerController : MonoBehaviour
     private GameObject focalPoint;
     public float speed;
     public bool hasPowerup = false;
+    private float powerupStrength = 15.0f;
+    public GameObject PowerupIndicator;
     // Start is called before the first frame update
+    
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
@@ -16,18 +19,46 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
+    
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
 
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+        PowerupIndicator.transform.position = transform.position + new Vector3(0, -.5f, 0);
     }
+    
+    
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Powerup")){
+        if(other.CompareTag("Powerup"))
+        {
             hasPowerup = true;
             Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine()); 
+            PowerupIndicator.gameObject.SetActive(true);
         }
+    
+    
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        PowerupIndicator.gameObject.SetActive(false);
+    }
 
+    }
+   
+   
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup){
+
+            Rigidbody EnemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+            
+            EnemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+            Debug.Log("Player collided with" + collision.gameObject + "with powerup set to" + hasPowerup);
+            
+        }
     }
 }
